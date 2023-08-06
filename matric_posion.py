fbe4c6e2c230afe101eb74b0e2e78bdbbe91f307
ashok <ak4132592ashok@gmail.com>
2023-08-06 16:48:24 +0530

    matric_posion_set_value

---
+++ matric_posion.py
@@ -0,0 +1,20 @@
+#        1     2     3
+
+lis1 = [" ", " ", " "]
+lis2 = [" ", " ", " "]
+lis3 = [" ", " ", " "]
+
+map = [lis1, lis2, lis3]
+print(map)
+print(f" {lis1}\n {lis2} \n {lis3}")
+
+set_position = input("enter position: ")
+value = input("Enter value for this index: ")
+horizontal = int(set_position[0])
+vertical = int(set_position[1])
+
+set_row = map[vertical-1]
+set_col = set_row[horizontal-1]
+set_row.append(value)
+
+print(f" {lis1}\n {lis2} \n {lis3}")
