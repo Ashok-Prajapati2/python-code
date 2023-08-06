976200869b9b9d6d00806192538b688478b11890
ashok <ak4132592ashok@gmail.com>
2023-08-06 12:35:52 +0530

    random-list-chouse

--- second.py
+++ second.py
@@ -1,2 +1,29 @@
 
 pi = 3.14
+
+# list
+
+list_is = ['item1','item2',5326]
+print(list_is)
+
+list_is.append("item append")
+print(list_is)
+
+   
+list_is[1] = 'item change'
+print(list_is)
+
+list_is.extend("Ashok")
+print(list_is)
+
+
+for item in list_is:
+    print(item)
+
+
+name_is = input("Enter name of your friend: ")
+
+my_list = name_is.split()
+
+print(my_list)
+
