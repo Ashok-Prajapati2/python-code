9c820d1d70316d178ff00cb82d93a386f20ba818
ashok <ak4132592ashok@gmail.com>
2023-08-06 22:37:15 +0530

    matric_posion_set-value

--- project/matric_posion.py
+++ project/matric_posion.py
@@ -1,8 +1,8 @@
 #        1     2     3
 
-lis1 = [" ", " ", " "]
-lis2 = [" ", " ", " "]
-lis3 = [" ", " ", " "]
+lis1 = ["1", "4", "5"]
+lis2 = ["2", "6", "7"]
+lis3 = ["3", "8", "9"]
 
 map = [lis1, lis2, lis3]
 print(map)
@@ -13,8 +13,10 @@ value = input("Enter value for this index: ")
 horizontal = int(set_position[0])
 vertical = int(set_position[1])
 
-set_row = map[vertical-1]
-set_col = set_row[horizontal-1]
-set_row[horizontal-1] = value
-
+set_row = map[horizontal-1]
+# print(set_row)
+set_col = set_row[vertical-1]
+# print(set_col)
+set_row[vertical-1] = value
+# print(set_row[horizontal-1])
 print(f" {lis1}\n {lis2} \n {lis3}")
