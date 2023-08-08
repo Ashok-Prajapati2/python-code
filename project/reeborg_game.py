7a0fc467fdde69e15d23827d55255ea515a2d424
ashok <ak4132592ashok@gmail.com>
2023-08-08 22:47:11 +0530

    game_http://reeborg.ca/world.html

---
+++ project/reeborg_game.py
@@ -0,0 +1,22 @@
+# always work with modify
+#http://reeborg.ca/world.html
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# x = 0 
+# while not object_here():
+# #while x < 435:
+#     x += 1
+#     if wall_in_front():
+#         turn_left()
+#         if wall_in_front():
+#             turn_right()
+#             turn_right()
+    
+#     else:
+#         move()
+#     if  right_is_clear():
+#         turn_right()
+# print(x)
