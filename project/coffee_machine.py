cd4986d81274135573652ce96d6d1aa21a97ebdd
ashok <ak4132592ashok@gmail.com>
2023-10-16 13:21:38 +0530

    coffee_machine

--- project/coffee_machine.py
+++ project/coffee_machine.py
@@ -1,82 +1,85 @@
-MENU= {
-    "espress" : {
-        "ingrediets" : {
-            "water" : 50,
-            "milk" : 50,
-            "coffee": 8 
-        } 
-        , 'cost' : 15
+# Define the available drinks in the MENU dictionary, including their ingredients and cost.
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 50,
+            "coffee": 8
+        },
+        'cost': 15
     },
-    "latte" : {
-        "ingrediets" : {
-            "water" : 200,
-            "milk" : 150,
-            "coffee": 25 
-        } 
-        , 'cost' : 25
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 25
+        },
+        'cost': 25
     },
-    "cappuccino" : {
-        "ingrediets" : {
-            "water" : 300,
-            "milk" : 200,
+    "cappuccino": {
+        "ingredients": {
+            "water": 300,
+            "milk": 200,
             "coffee": 24
-        } 
-        , 'cost' : 35
+        },
+        'cost': 35
     },
 }
 
+# Initialize the profit variable as a float (0.0).
 profit = 0.0
 
-resourees = {
-            "water" : 500,
-            "milk" :  300,
-            "coffee": 100,
-            
+# Initialize the resources available for making coffee.
+resources = {
+    "water": 500,
+    "milk": 300,
+    "coffee": 100,
 }
 
-
+# Define a function to check if there are sufficient resources for a given drink.
 def is_resouress_sufficient(drink):
-    drink = drink['ingrediets']
+    drink = drink['ingredients']
     for item in drink:
-        if resourees[item] <= drink[item]:
+        if resources[item] <= drink[item]:
             print(f"{item} is not sufficient")
-            return False 
+            return False
     return True
-   
-def  payment_succeful(payment):
+
+# Define a function to check if the payment is sufficient and calculate change.
+def payment_succeful(payment):
     if payment >= drink['cost']:
-        print(f"your change {payment - drink['cost']}")
+        print(f"Your change: ${payment - drink['cost']}")
         return True
     else:
         print("Sorry, that's not enough money. Money refunded.")
         return False
 
+# Define a function to make the selected coffee, deducting resources, and returning the cost of the drink.
 def make_coffee(drink):
-    
-    for item in drink['ingrediets']:
-        resourees[item] = resourees[item]-drink['ingrediets'][item]
+    for item in drink['ingredients']:
+        resources[item] = resources[item] - drink['ingredients'][item]
     print(f"Here is your {choice}. Enjoy!")
     return drink['cost']
 
+# Initialize a loop to run the coffee machine program.
 is_it = True
 
 while is_it:
-    choice = input("what you like (espress ,latte,cappuccino ) : ")
-    
+    choice = input("What would you like (espresso, latte, cappuccino): ")
+
     if choice == "off":
         is_it = False
     elif choice == "report":
-        print(resourees)
-        print(f"profit is ${profit}")
+        # Display the current resource quantities and profit.
+        print(resources)
+        print(f"Profit is ${profit:.2f}")
     else:
+        # Get the selected drink from the MENU.
         drink = MENU[choice]
-        
-        
+
         if is_resouress_sufficient(drink):
-            
-            payment = int(input("Make payment  : "))
+            payment = int(input("Make payment: "))
             
             if payment_succeful(payment):
-               profit += make_coffee(drink)
-                
-            
+                # Update the profit and deduct resources when a successful payment is made.
+                profit += make_coffee(drink)
